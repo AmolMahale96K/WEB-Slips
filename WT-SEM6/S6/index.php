ade408eb9f54bb1b0585275f856cf6d9bab3c937
AmolMahale96K <149690473+AmolMahale96K@users.noreply.github.com>
2024-04-10 08:11:12 +0530

    Add files via upload

---
+++ WT-SEM6/S6/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $doc = simplexml_load_file("book.xml");
+    $books = $doc->book;
+    foreach($books as $book)
+    {
+        echo "<br><br>Book Title : ".$book->title;
+        echo "<br>Book Author : ".$book->author;
+        echo "<br>Book Year : ".$book->year;
+        echo "<br>Book Price : ".$book->price;
+        echo "<br>Book pages : ".$book["pages"];
+    }
+
+?>
